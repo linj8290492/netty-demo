0a9a3bbf4778aa097bfafea9016800594702a4bc
linjun <664678659@qq.com>
2020-04-20 20:39:10 +0800

    初始化netty-nio

---
+++ netty-nio/src/main/java/com/netty/nio/NettyNioApplication.java
@@ -0,0 +1,13 @@
+package com.netty.nio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyNioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyNioApplication.class, args);
+	}
+
+}
