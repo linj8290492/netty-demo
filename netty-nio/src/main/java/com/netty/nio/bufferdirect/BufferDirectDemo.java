7d75ffa1d4a9804042d726fa0d41fec0785264f5
linjun <664678659@qq.com>
2020-04-20 22:00:29 +0800

    直接缓冲区

---
+++ netty-nio/src/main/java/com/netty/nio/bufferdirect/BufferDirectDemo.java
@@ -0,0 +1,38 @@
+package com.netty.nio.bufferdirect;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author deke
+ * @description 直接缓冲区
+ * @date 2020/4/20
+ */
+public class BufferDirectDemo {
+    public static void main(String[] args) throws Exception {
+        /**读取文件中的信息*/
+        FileInputStream fin = new FileInputStream("D:\\myself\\file\\netty-nio1.txt");
+        /**创建文件的操作管道*/
+        FileChannel fc = fin.getChannel();
+
+        /**写入这个文件*/
+        FileOutputStream fout = new FileOutputStream("D:\\myself\\file\\netty-nio2.txt");
+        /**创建文件的操作管道*/
+        FileChannel fcout = fout.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+        while (true){
+            buffer.clear();
+            int i = fc.read(buffer);
+            if(i==-1){
+                break;
+            }
+            buffer.flip();
+            fcout.write(buffer);
+        }
+
+    }
+}
